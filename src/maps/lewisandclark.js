c4e24aecef41018e0f701977980e44d59a3d3ced
Sean Ray <sean.ray@wwt.com>
2015-02-12 23:22:18 -0600

    Save and list markers

--- src/maps/lewisandclark.js
+++ src/maps/lewisandclark.js
@@ -7,6 +7,8 @@
     var layers = [];
     var map;
     var current = 0;
+    var markers;
+    var layers;
 
     init();
 
@@ -33,19 +35,58 @@
                     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
                     maxZoom: 18
                 }).addTo(map);
+
         }
+
+        $.ajax({
+            type: "GET",
+            url: 'http://localhost:8080/markers',
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function(markers) {
+                console.log(markers);
+                addMarkers(markers);
+            }
+        });
 
         map.on('draw:created', function (e) {
             var type = e.layerType,
                 layer = e.layer;
 
+            console.log(e.layer.getLatLng());
             if (type === 'marker') {
+                $.ajax({
+                    type: "POST",
+                    url: 'http://localhost:8080/markers',
+                    data: JSON.stringify({latLng: [layer.getLatLng().lat, layer.getLatLng().lng]}),
+                    contentType: "application/json; charset=utf-8",
+                    dataType: "json",
+                    success: function() {console.log('created');}
+                });
                 // Do marker specific actions
             }
 
             // Do whatever else you need to. (save to db, add to map etc)
             map.addLayer(layer);
         });
+    }
+
+
+    function addMarkers(markers) {
+        removeLayers();
+
+        for(var i=0; i < markers.length; i++) {
+            layers.push(new L.marker(markers[i].latLng));
+            map.addLayer(layers[i]);
+        }
+    }
+
+    function removeLayers() {
+        for(var i=0; i < layers.length; i++) {
+            map.removeLayer(layers[i]);
+        }
+
+        layers = [];
     }
 
 }(L, MQ));
