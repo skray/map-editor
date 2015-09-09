d080bf45777ea8b5b3c01c0629c39f12569c2c74
Sean Ray <seankennethray@gmail.com>
2015-09-08 23:45:25 -0500

    Added some buttons to test out read/write with auth

--- src/index.js
+++ src/index.js
@@ -1,5 +1,32 @@
 var mapapi = require('./maps/mapapi');
 var Transparency = require('transparency');
+var Firebase = require('firebase');
+var ref = new Firebase('https://amber-inferno-2147.firebaseio.com/');
+
+document.getElementById('login').addEventListener('click', function onLoginClick() {
+	ref.authWithOAuthPopup("github", function(error, authData) {
+	  if (error) {
+	    console.log("Login Failed!", error);
+	  } else {
+	    console.log("Authenticated successfully with payload:", authData);
+	  }
+	});	
+});
+
+document.getElementById('logout').addEventListener('click', function onLogoutClick() {
+	ref.unauth();
+});
+
+document.getElementById('save').addEventListener('click', function saveData() {
+	ref.set("I'm writing data", function(error) {
+	  if (error) {
+	    alert("Data could not be saved." + error);
+	  } else {
+	    alert("Data saved successfully.");
+	  }
+	});
+});
+
 
 init();
 
